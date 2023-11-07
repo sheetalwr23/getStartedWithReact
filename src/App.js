8d1f7140bf374ccdccc69d48bf10a93452168137
Sheetal <Sheetalwr23@gmail.com>
2023-11-07 11:35:31 +0530

    task 9 Passing Data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import ExpenseItem from './components/ExpenseItem'; 
 
+import "./components/ExpenseItem.css"
+
 function App() {
+
+
+
+  const items=[ {expenseDate : new Date(2023, 9, 13),
+     expenseTitle : "Food",
+     expenseAmount : 50,
+     locationOfExpenditure:"Hotel" } ,
+     {expenseDate : new Date(2023, 9, 13),
+      expenseTitle : "Petrol",
+      expenseAmount : 100,
+      locationOfExpenditure:"Petrol Pump" },
+      {expenseDate : new Date(2023, 9, 13),
+        expenseTitle : "Movies",
+        expenseAmount : 200,
+        locationOfExpenditure:"Inox" }
+    ]
+
   return (
-    <div>    
-      <ExpenseItem></ExpenseItem>    
+    <div> 
+      <div className="expense-item">
+        <div className="expense-item__description">
+          <h1>Expense Item!</h1>
+        </div>
+      </div>
+      {items.map((item, index) => (
+        <ExpenseItem
+          key={index}
+          expenseTitle={item.expenseTitle}
+          expenseAmount={item.expenseAmount}
+          expenseDate={item.expenseDate}
+          locationOfExpenditure={item.locationOfExpenditure}
+        />
+      ))}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,57 +1,20 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2023, 9, 13);
-  const expenseTitle = "Food";
-  const expenseAmount = 10;
-  const locationOfExpenditure="Hotel"
-  
-  const formattedDate = expenseDate.toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  });
+function ExpenseItem(props) {
 
   return (
-    <div >
-      <div className="expense-item">
-        <div className="expense-item__description">
-          <h1>Expense Item!</h1>
-        </div>
-      </div>
+    <div >      
 
       <div className="expense-item"> 
-        <div>{formattedDate}</div>
+        <div>{props.expenseDate.toISOString()}</div>
         <div className="expense-item__description">
-          <h3>{expenseTitle}</h3>      
-          <h3>{locationOfExpenditure}</h3>   
+          <h3>{props.expenseTitle}</h3>      
+          <h3>{props.locationOfExpenditure}</h3>   
         <div className="expense-item__price">
-          <p>Rs{expenseAmount}</p>
+          <p>Rs{props.expenseAmount}</p>
         </div>
         </div>
-      </div>
-      <div className="expense-item"> 
-        <div>{formattedDate}</div>
-        <div className="expense-item__description">
-          <h3>{expenseTitle}</h3>  
-          <h3>{locationOfExpenditure}</h3>       
-        <div className="expense-item__price">
-          <p>Rs{expenseAmount}</p>
-        </div>
-        </div>
-      </div>
-      <div className="expense-item"> 
-        <div>{formattedDate}</div>
-        <div className="expense-item__description">
-          <h3>{expenseTitle}</h3>   
-          <h3>{locationOfExpenditure}</h3>      
-        <div className="expense-item__price">
-          <p>Rs{expenseAmount}</p>
-        </div>
-        </div>
-      </div>
-     
-    
+      </div>   
     </div>
   );
 }
