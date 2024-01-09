ab30dfd32736431f7dafe44a8a6a6dc63a940d5f
Sheetal <Sheetalwr23@gmail.com>
2024-01-09 15:04:54 +0530

    task 1 add event listener

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,21 @@
 import React from "react";
 import "./ExpenseItem.css";
-import ExpenseDate from './ExpenseDate';
+import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/card";
 
-
-const ExpenseItem=(props)=> {
-    return (       
-      <Card className="expense-item"> 
-        <ExpenseDate date={props.date}/>
-        <div className='expense-item__description'>
+const ExpenseItem = (props) => {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
         <h2>{props.title}</h2>
         <h2>{props.locationOfExpenditure}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-        </div>
-      </Card>    
+        <div className="expense-item__price">${props.amount}</div>
+        <button className="expense-item__delete" onClick={deleteButton}>
+          Delete
+        </button>
+      </div>
+    </Card>
   );
-}
-
+};
 export default ExpenseItem;
